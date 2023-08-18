fe9fbd2d6959688a57537b23de2c8c6b0c59e8d9
MUSKAN <64833214+Muskan-Git-Code@users.noreply.github.com>
2023-08-18 22:06:21 +0530

    Add files via upload

--- src/main/java/com/hackerrank/stereotypes/repository/UserRepository.java
+++ src/main/java/com/hackerrank/stereotypes/repository/UserRepository.java
@@ -5,6 +5,9 @@ import com.hackerrank.stereotypes.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
 }
